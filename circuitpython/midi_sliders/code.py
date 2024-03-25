f5422c85e4af333709a928bfc20436f68739fd07
Tod Kurt <todbot@users.noreply.github.com>
2024-03-25 11:50:03 -0700

    circuitpython midi_sliders demo

---
+++ circuitpython/midi_sliders/code.py
@@ -0,0 +1,109 @@
+#
+# picoslidertoy_midi_sliders_code.py -- turn picoslidertoy into MIDI controller
+# 2024 - @todbot / Tod Kurt - github.com/todbot/picoslidertoy
+#
+# based off of "picotouch_test.py":
+# picotouch_test.py -- Testing for tiny capsense controller using Pico,
+# 2021 - @todbot / Tod Kurt - github.com/todbot/picotouch
+#
+# To use:
+#
+# 1. Install needed libraries:
+#   circup install adafruit_displayio_ssd1306 adafruit_midi
+# 2. Copy over files in this directory to CIRCUITPY:
+#   cp circuitpython/midi_sliders/* /Volumes/CIRCUITPY/
+#
+
+import time
+import board
+import touchio
+import busio
+import displayio
+import adafruit_displayio_ssd1306
+import usb_midi
+import adafruit_midi
+from adafruit_midi.control_change import ControlChange
+from adafruit_midi.note_on import NoteOn
+from adafruit_midi.note_off import NoteOff
+
+from touchslider import TouchWheel, TouchSlider
+from slider_display import FaderDisplay, WheelDisplay, PadsDisplay
+
+# midi cc and note definitions
+midi_ccs = [ 74, 1, 72, 74, 71 ]
+midi_notes = [ 34, 35, 36, 37,  38, 39, 40, 41,  24 ]  # FIXME
+midi_chan = 1
+
+# pin definitions
+faderA_pins = (board.GP4, board.GP0, board.GP28)
+faderB_pins = (board.GP5, board.GP1, board.GP27)
+faderC_pins = (board.GP3, board.GP2, board.GP26)
+wheelX_pins = (board.GP7, board.GP8, board.GP9)
+wheelY_pins = (board.GP10, board.GP11, board.GP12)
+pad_pins = (board.GP22, board.GP21, board.GP20, board.GP19,
+            board.GP18, board.GP17, board.GP16, board.GP6, board.GP13)
+i2c_sda_pin, i2c_scl_pin = board.GP15, board.GP14
+
+# set up the touch controls
+sliders = (
+    TouchSlider(faderA_pins),
+    TouchSlider(faderB_pins),
+    TouchSlider(faderC_pins),
+    TouchWheel(wheelX_pins, offset=0.25),
+    TouchWheel(wheelY_pins, offset=0.25),
+)
+pads = [touchio.TouchIn(pin) for pin in pad_pins]
+
+# virtual on-screen displays of controls
+slider_displays = (
+    FaderDisplay(10 + 15*0, 10, 8, 35),  # fixme: need ".pos(pos-0.5)"
+    FaderDisplay(10 + 15*1, 10, 8, 35),
+    FaderDisplay(10 + 15*2, 10, 8, 35),
+    WheelDisplay(70 + 0*35, 25, 15, knob_w=9, phase_offset=-0.25),
+    WheelDisplay(70 + 1*35, 25, 15, knob_w=9, phase_offset=-0.25),
+)
+pad_displays = PadsDisplay(20, 50, n=9, pad_h=10, pad_w=10)
+
+# set up midi out
+midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=midi_chan-1)
+
+# set up the display
+dw,dh = 128, 64
+displayio.release_displays()
+i2c = busio.I2C(scl=i2c_sda_pin, sda=i2c_scl_pin, frequency=1_000_000)
+display_bus = displayio.I2CDisplay(i2c, device_address=0x3c)
+display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=dw, height=dh)
+maingroup = displayio.Group()
+display.root_group = maingroup
+
+for s in slider_displays:
+    maingroup.append(s)
+maingroup.append(pad_displays)
+
+# used to keep track of which note pads are curerntly pressed
+pad_state = [False] * len(pads)
+
+while True:
+    for i,slider in enumerate(sliders):
+        pos = slider.pos()
+        if pos is not None:   # touched!
+            slider_displays[i].pos(pos)   # fixme: need ".pos(pos-0.5)" for fader
+            slider_displays[i].touch(True)
+            cc_val = int((1-pos) * 127)
+            midi.send(ControlChange(midi_ccs[i], cc_val))
+            print("slider:%d: %1.2f cc:%d ccval:%d" % (i,pos,midi_ccs[i],cc_val))
+        else:
+            slider_displays[i].touch(False)
+            
+    for i, pad in enumerate(pads):
+        v = pad.value
+        pad_displays.set(i, v)
+        if pad_state[i] != v:
+            n = midi_notes[i]
+            msg = NoteOn(n, 127) if v else NoteOff(n,0)
+            midi.send(msg)
+            pad_state[i] = v
+            print("pad:%d: note:" % i, msg)
+
+    time.sleep(0.01)
+
---
+++ circuitpython/midi_sliders/touchslider.py
@@ -0,0 +1,77 @@
+## pylint: disable=invalid-name
+# SPDX-FileCopyrightText: Copyright (c) 2024 Tod Kurt
+# SPDX-License-Identifier: MIT
+"""
+`touchslider`
+================================================================================
+
+Create linear (TouchSlider) and rotary (TouchWheel) capacative touch sliders
+ using three `touchio` pins and special pad geometry. 
+
+Originally part of the 'touchwheels' project: https://github.com/todbot/touchwheels/
+2023 - @todbot / Tod Kurt
+
+"""
+
+import touchio
+
+class TouchWheel():
+    """Simple capacitive touchweel made from three captouch pads """
+    #def __init__(self, touch_pins, offset = -0.333 * (3/4), sector_scale=0.333, wrap_value=True):
+    def __init__(self, touch_pins, offset = 0, sector_scale=0.333, wrap_value=True):
+        # note: sector_scale & offset determined from len(touch_pins)?
+        self.touchins = [touchio.TouchIn(p) for p in touch_pins]
+        self.offset = offset  # physical design is rotated anti-clockwise
+        self.scale = sector_scale
+        self.wrap_value = wrap_value
+        self.last_pos = 0
+        self.pos_filt = 0.7
+
+    def pos(self):
+        """
+        Given three touchio.TouchIn pads, compute wheel position 0-1
+        or return None if wheel is not pressed
+        """
+        a = self.touchins[0]
+        b = self.touchins[1]
+        c = self.touchins[2]
+
+        # compute raw percentages of how much each pad is touched
+        a_pct = (a.raw_value - a.threshold) / a.threshold
+        b_pct = (b.raw_value - b.threshold) / b.threshold
+        c_pct = (c.raw_value - c.threshold) / c.threshold
+        #print( "%+1.2f  %+1.2f  %+1.2f" % (a_pct, b_pct, c_pct), end="\t")
+
+        pos = None
+
+        # cases when finger is touching two pads
+        if a_pct >= 0 and b_pct >= 0:  #
+            pos = self.scale * (0 + (b_pct / (a_pct + b_pct)))
+        elif b_pct >= 0 and c_pct >= 0:  #
+            pos = self.scale * (1 + (c_pct / (b_pct + c_pct)))
+        elif c_pct >= 0 and a_pct >= 0 and self.wrap_value:  # i.e. is wheel
+            pos = self.scale * (2 + (a_pct / (c_pct + a_pct)))
+
+        # special cases when finger is just on a single pad.
+        elif a_pct > 0 and b_pct <= 0 and c_pct <= 0:
+            pos = 0 * self.scale
+        elif a_pct <= 0 and b_pct > 0 and c_pct <= 0:
+            pos = 1 * self.scale
+        elif a_pct <= 0 and b_pct <= 0 and c_pct > 0 and self.wrap_value:
+            pos = 2 * self.scale
+
+        if pos is not None:  # i.e. if touched
+            # filter noise
+            if abs(pos - self.last_pos) > 0.5:
+                print("bonk", pos, self.last_pos)
+                self.last_pos = pos
+            #pos = self.pos_filt * pos + (1-self.pos_filt) * self.last_pos
+            self.last_pos = pos
+            # wrap pos around the 0-1 circle if offset puts it outside that range
+            pos = (pos + self.offset) % 1
+        return pos
+    
+class TouchSlider(TouchWheel):
+    """A TouchSlider is a linearized TouchWheel """
+    def __init__(self, touch_pins, offset=0, sector_scale=0.5, wrap_value=False):
+        super().__init__(touch_pins, offset, sector_scale, wrap_value)
